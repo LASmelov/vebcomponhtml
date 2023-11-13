2c3efae245ce9e23def57d0f7472b471b960c8ea
LASmelov <smelov020@gmail.com>
2023-11-13 17:50:51 +0300

    first commit

---
+++ form-component.js
@@ -0,0 +1,106 @@
+// 4 задание
+
+// class CustomForm extends HTMLElement{
+//     constructor(){
+//         super();
+//         const shadow = this.attachShadow({mode: 'open'});
+//         const form = document.createElement('form');
+
+//         const nameInput = document.createElement('input');
+//         nameInput.type = 'text';
+//         nameInput.placeholder = 'Имя';
+
+//         const name2Input = document.createElement('input');
+//         name2Input.type = 'text';
+//         name2Input.placeholder = 'Отчество';
+
+//         const name3Input = document.createElement('input');
+//         name3Input.type = 'text';
+//         name3Input.placeholder = 'Фамилия';
+
+//         const emailInput = document.createElement('input');
+//         emailInput.type = 'email';
+//         emailInput.placeholder = 'Email';
+
+    
+
+//         const submitButton = document.createElement('button');
+//         submitButton.textContent = 'Отправить';
+
+//         const submitButton2 = document.createElement('button');
+//         submitButton2.textContent = 'Отмена';
+
+//         const submitButton3 = document.createElement('button');
+//         submitButton3.textContent = 'Повторить';
+
+//         form.style.width = '600px';
+//         form.style.padding = '20px';
+//         form.style.border = '1px solid red';
+//         form.style.borderRadius = '5px';
+//         form.style.boxShadow = '0 2px 5px rgba(0, 0, 0, 0.1)';
+
+//         nameInput.style.display = 'block';
+//         nameInput.style.width = '550px';
+//         nameInput.style.marginBottom = '10px';
+//         nameInput.style.padding = '10px';
+
+//         name2Input.style.display = 'block';
+//         name2Input.style.width = '550px';
+//         name2Input.style.marginBottom = '10px';
+//         name2Input.style.padding = '10px';
+
+//         name3Input.style.display = 'block';
+//         name3Input.style.width = '550px';
+//         name3Input.style.marginBottom = '10px';
+//         name3Input.style.padding = '10px';
+
+//         emailInput.style.display = 'block';
+//         emailInput.style.width = '550px';
+//         emailInput.style.marginBottom = '10px';
+//         emailInput.style.padding = '10px';
+
+//         submitButton.style.backgroundColor = 'black';
+//         submitButton.style.color = 'pink';
+//         submitButton.style.border = 'none';
+//         submitButton.style.padding = '10px 20px';
+//         submitButton.style.cursor = 'pointer';
+//         submitButton.style.marginRight = '10px';
+
+//         submitButton2.style.backgroundColor = 'black';
+//         submitButton2.style.color = 'pink';
+//         submitButton2.style.border = 'none';
+//         submitButton2.style.padding = '10px 20px';
+//         submitButton2.style.cursor = 'pointer';
+//         submitButton2.style.marginRight = '10px';
+
+//         submitButton3.style.backgroundColor = 'black';
+//         submitButton3.style.color = 'pink';
+//         submitButton3.style.border = 'none';
+//         submitButton3.style.padding = '10px 20px';
+//         submitButton3.style.cursor = 'pointer';
+        
+
+//         form.appendChild(nameInput);
+//         form.appendChild(name2Input);
+//         form.appendChild(name3Input);
+//         form.appendChild(emailInput);
+//         form.appendChild(submitButton);
+//         form.appendChild(submitButton2);
+//         form.appendChild(submitButton3);
+        
+
+//         shadow.appendChild(form);
+//     }
+// }
+
+// customElements.define('custom-form', CustomForm);
+
+// кт 1 с булгакова
+
+
+
+
+// 5 задание
+
+
+// задание 6 
