00aeefb0ec420538e26fe3f5522365e1be69abf8
LASmelov <smelov020@gmail.com>
2023-11-28 21:30:24 +0300

    add

--- main.js
+++ main.js
@@ -216,3 +216,108 @@
 
 // // Инициализируем отображение расходов
 // updateExpenses();
+
+
+
+// задание 14
+// class ExpenseTrackerLite extends HTMLElement {
+//     constructor() {
+//         super();
+//         this.expenses = [];
+//         this.total = 0;
+
+//         this.attachShadow({ mode: 'open' });
+//         this.render();
+//         this.setupEventListeners();
+//     }
+
+//     render() {
+//         this.shadowRoot.innerHTML = `
+//         <div id="app">
+//             <label for="expense-name">Название покупки:</label>
+//             <input type="text" id="expense-name" required>
+
+//             <label for="expense-amount">Стоимость:</label>
+//             <input type="number" id="expense-amount" required>
+//             <button id="add-expense">Добавить покупку</button>
+
+//             <ul id="expense-list"></ul>
+//             <p>Общая сумма: <span id="total-amount">0</span></p>
+//         </div> `;
+//     }
+
+//     setupEventListeners() {
+//         const addButton = this.shadowRoot.getElementById('add-expense');
+
+//         addButton.addEventListener('click', () => {
+//             this.addExpense();
+//         });
+//     }
+
+//     addExpense() {
+//         const nameInput = this.shadowRoot.getElementById('expense-name');
+//         const amountInput = this.shadowRoot.getElementById('expense-amount');
+//         const expenseList = this.shadowRoot.getElementById('expense-list');
+//         const totalAmount = this.shadowRoot.getElementById('total-amount');
+
+//         const name = nameInput.value;
+//         const amount = parseFloat(amountInput.value);
+
+//         if (name && !isNaN(amount)) {
+//             this.expenses.push({ name, amount });
+//             this.total += amount;
+
+//             this.renderExpenses();
+//             totalAmount.textContent = this.total.toFixed(2);
+
+//             nameInput.value = '';
+//             amountInput.value = '';
+//         }
+//     }
+
+//     renderExpenses() {
+//         const expenseList = this.shadowRoot.getElementById('expense-list');
+//         const li = document.createElement('li');
+//         const lastExpense = this.expenses[this.expenses.length - 1];
+//         li.textContent = `${lastExpense.name}: ${lastExpense.amount.toFixed(2)}`;
+//         expenseList.appendChild(li);
+//     }
+// }
+
+// customElements.define('expense-tracker-lite', ExpenseTrackerLite);
+
+// задание 15
+
+// class CustomInput extends HTMLElement {
+//     constructor() {
+//         super()
+//         const shadow = this.attachShadow({ mode: 'open' })
+//         const style = document.createElement('style')
+//         style.textContent = `
+//         :host{
+//             display:block;
+//         }
+
+//         input {
+//             width: 100%;
+//             padding: 8px;
+//             border: 1px solid var(--input-text-color);
+//             border-radius: var(--input-border-radius);
+//             background-color: var(--input-backround);
+//             color: var(--input-text-color);
+//             box-sizing: border-box;
+//             font-size: 16px;
+//         }
+
+//         ::placeholder{
+//             color: var(--placeholder-color);
+//         }
+//         `
+//         const input = document.createElement('input')
+//         input.setAttribute('type', 'text');
+//         input.setAttribute('placeholder', 'Напиши что-то тут')
+//         shadow.appendChild(style)
+//         shadow.appendChild(input)
+//     }
+// }
+// customElements.define('custom-input', CustomInput)
